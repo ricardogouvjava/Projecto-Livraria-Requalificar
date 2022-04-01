94d089c006bf1ea9b137a9287e6e9f9ce865f31d
ricardogouvjava <ricardogouvjava@gmail.com>
2022-04-01 19:27:13 +0100

    Revert "...."
    This reverts commit cba65aa11a9a8968a58dc76497bf5756f7cac5e0.

--- backend/src/main/java/requalificar/projecto/models/Editora.java
+++ backend/src/main/java/requalificar/projecto/models/Editora.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 import javax.persistence.*;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Entity(name="Editora")
 @Table(name="editora")
 public class Editora 
